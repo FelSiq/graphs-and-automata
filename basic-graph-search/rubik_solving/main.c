d11f95b417044f0adde148715dd196df909c74ca
FelSiq <felipe.siqueira@usp.br>
2018-09-26 06:46:09 -0300

    Some changes to main

--- basic-graph-search/rubik_solving/main.c
+++ basic-graph-search/rubik_solving/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char *const argv[]) {
 	if (argc < ARGV_ARGSNUM - 1) {
-		printf("usage: %s <filepath>", argv[ARGV_PROGNAME]);
+		printf("usage: %s <filepath>\n", argv[ARGV_PROGNAME]);
 		return 1;
 	}
 
@@ -15,7 +15,21 @@ int main(int argc, char *const argv[]) {
 		return 2;
 	}
 
-	printf("Loaded configuration:\n");
+	#if DEBUG
+		rubik_rotate(r, C_W, DIR_CLKWISE);
+		rubik_rotate(r, C_R, DIR_C_CLKWISE);
+		rubik_rotate(r, C_B, DIR_CLKWISE);
+		//rubik_rotate(r, C_Y, DIR_CLKWISE);
+		//rubik_rotate(r, C_Y, DIR_CLKWISE);
+		//rubik_rotate(r, C_O, DIR_C_CLKWISE);
+		//rubik_rotate(r, C_R, DIR_CLKWISE);
+		//rubik_rotate(r, C_G, DIR_C_CLKWISE);
+		//rubik_rotate(r, C_O, DIR_C_CLKWISE);
+		//rubik_rotate(r, C_O, DIR_C_CLKWISE);
+		rubik_reinit(r);
+	#endif
+
+	printf("Start configuration:\n");
 	rubik_print(r);
 
 	printf("Solving...\n");
