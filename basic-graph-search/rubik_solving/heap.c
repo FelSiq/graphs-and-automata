eef0b0fdca38fdc3d3f4e6d227b456a68723b3ff
FelSiq <felipe.siqueira@usp.br>
2018-09-26 06:45:59 -0300

    Fixed minheap implementation

--- basic-graph-search/rubik_solving/heap.c
+++ basic-graph-search/rubik_solving/heap.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <heap.h>
 
+#define HEAP_DEBUG 0
+
 typedef struct {
 	void *item;
 	float key;
@@ -20,17 +22,120 @@ heap *heap_start() {
 	}
 	return h;
 }
+
 void *heap_pop(heap *h) {
-	return NULL;
+	heap_node *heap_head = h->heap;
+	void *item = heap_head->item;
+
+	register unsigned long cur_position = 0, min_index,
+		lchild_index = HEAP_LCHILD(cur_position), 
+		rchild_index = HEAP_RCHILD(cur_position);
+	register const unsigned long cur_size = --h->size;
+
+	heap_node aux;
+
+	// Swap head node with tail node
+	aux = h->heap[0];
+	h->heap[0] = h->heap[cur_size];
+	h->heap[cur_size] = aux;
+
+	h->heap = realloc(h->heap, sizeof(heap_node) * cur_size);
+
+	// Downgrade "new head node" to its respective place
+	while (lchild_index < cur_size) {
+
+		if (rchild_index < cur_size && 
+			h->heap[rchild_index].key < h->heap[lchild_index].key) {
+			min_index = rchild_index;
+		} else {
+			min_index = lchild_index;
+		}
+
+		if (h->heap[min_index].key < h->heap[cur_position].key) {
+			// If min_index element has a smaller key than the
+			// current one, swap.
+			aux = h->heap[cur_position];
+			h->heap[cur_position] = h->heap[min_index];
+			h->heap[min_index] = aux;
+
+			cur_position = min_index;
+			lchild_index = HEAP_LCHILD(cur_position), 
+			rchild_index = HEAP_RCHILD(cur_position);
+		} else {
+			// Else, break main loop
+			lchild_index = cur_size;
+		}
+	}
+
+	return item;
 }
 
 void heap_push(heap *h, float key, void *item) {
+	const register unsigned long int cur_size = h->size;
+	register unsigned long int cur_position = cur_size;
 
+	h->heap = realloc(h->heap, sizeof(heap_node) * (cur_size + 1));
+	h->heap[cur_size].item = item;
+	h->heap[cur_size].key = key;
+
+	heap_node aux;
+	register heap_node
+		*master_node = h->heap + HEAP_MASTER(cur_position), 
+		*cur_node = h->heap + cur_position;
+
+	while (master_node->key > cur_node->key) {
+		aux = *cur_node;
+		*cur_node = *master_node;
+		*master_node = aux;
+
+		cur_position = HEAP_MASTER(cur_position);
+		master_node = h->heap + HEAP_MASTER(cur_position);
+		cur_node =  h->heap + cur_position;
+	} 
+
+	h->size++;
 }
 
 void heap_destroy(heap **h) {
 	if (h != NULL && *h != NULL) {
+		if ((*h)->heap != NULL) {
+			//while ((*h)->size--) {
+			//	if ((*h)->heap[(*h)->size].item != NULL)
+			//		free((*h)->heap[(*h)->size].item);
+			//}
+			free((*h)->heap);
+		}
 		free(*h);
 		*h = NULL;
 	}
 }
+
+#if HEAP_DEBUG == 1
+	int main(int argc, char *argv[]) {
+		heap *h = heap_start();
+
+		int *item;
+		float key = 1;
+		for (int i = 0; i < 6; i++) {
+			item = malloc(sizeof(int));
+			*item = i * i;
+			key = key * (-0.5) * (float) (1 + i);
+			heap_push(h, key, item);
+			printf("pushed (%f, %d)\n", key, *item);
+		}
+
+		for (int i = 0; i < 6; i++)
+			printf("[%d, %d]. (%f, %d)\n", HEAP_MASTER(i), i, 
+				h->heap[i].key, *(int *)h->heap[i].item);
+
+		for (int i =0; i < 6; i++) {
+			item = heap_pop(h);
+			printf("pop: %d\n", *item);
+			free(item);
+		}
+
+		heap_destroy(&h);
+
+		return 0;
+	}
+#endif
--- basic-graph-search/rubik_solving/heap.h
+++ basic-graph-search/rubik_solving/heap.h
@@ -1,6 +1,14 @@
 #ifndef __HEAP_H__
 #define __HEAP_H__
 
+#define HEAP_LCHILD(NODE) (1 + (NODE) * 2)
+#define HEAP_RCHILD(NODE) (2 + (NODE) * 2)
+#define HEAP_MASTER(NODE) (((NODE) > 0) ? (((NODE) - 1) / 2) : 0)
+
+#ifndef MIN
+	#define MIN(A,B) (((A) < (B)) ? (A) : (B))
+#endif
+
 typedef struct heap_struct heap;
 
 heap *heap_start();
