3abe97e5dcf2dd734db2d7d63d1f5613c41bc545
FelSiq <felipe.siqueira@usp.br>
2018-09-28 22:36:13 -0300

    Memory optimizations

--- basic-graph-search/rubik_solving/include/heap.h
+++ basic-graph-search/rubik_solving/include/heap.h
@@ -9,11 +9,13 @@
 	#define MIN(A,B) (((A) < (B)) ? (A) : (B))
 #endif
 
+#define HEAP_KEY(NODE) (*(float *)(NODE))
+
 typedef struct heap_struct heap;
 
 heap *heap_start();
-void *heap_pop(heap *h);
-void heap_push(heap *h, float key, void *item);
+void *heap_pop(heap *restrict h);
+void heap_push(heap *h, unsigned char *restrict key_and_move);
 void heap_destroy(heap **h);
 unsigned long int heap_size(heap const *restrict h);
 
